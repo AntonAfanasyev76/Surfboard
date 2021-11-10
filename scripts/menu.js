711dc662f5c79ac7720da73376e824640ff8f81a
Anton Afanasyev <anton-afanasyev@mail.ru>
2021-11-10 21:04:01 +0300

    добавил класс hidden

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector(".fullscreen-menu");
 const closeBtn = menu.querySelector(".fullscreen-menu__close");
+const body = document.querySelector('body');
 
 hamburger.addEventListener("click", e => {
   e.preventDefault();
   menu.classList.add("active");
+  body.classList.add("hidden");
 });
 
 closeBtn.addEventListener("click", e => {
   e.preventDefault();
   menu.classList.remove("active");
+  body.classList.remove("hidden");
 })
