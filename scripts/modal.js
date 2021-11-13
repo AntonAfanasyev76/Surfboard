ca4d89630c6450ec77507df0f6b470874edaee2d
Anton Afanasyev <anton-afanasyev@mail.ru>
2021-11-13 17:43:48 +0300

    AJAX

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,3 +1,16 @@
+const validateFields = (form, fieldsArray) => {
+    fieldsArray.forEach((field) => {
+        field.removeClass("input-error");
+        if(field.val() === "") {
+            field.addClass("input-error");
+        }
+    });
+
+    const errorFields = form.find(".input-error");
+
+    return errorFields.length === 0;
+}
+
 $('.form').submit(e => {
     e.preventDefault();
 
@@ -6,18 +19,25 @@ $('.form').submit(e => {
     const phone = form.find("[name='phone']");
     const comment = form.find("[name='comment']");
     const to = form.find("[name='to']");
+
+    const isValid = validateFields(form,[name, phone, comment, to]);
+
+
+    if(isValid){
+        $.ajax({
+            url: "https://webdev-api.loftschool.com/sendmail",
+            method: "post",
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                comment: comment.val(),
+                to: to.val(),
+            },
+        });
+    }
     
         
-    $.ajax({
-        url: "https://webdev-api.loftschool.com/sendmail",
-        method: "post",
-        data: {
-            name: name.val(),
-            phone: phone.val(),
-            comment: comment.val(),
-            to: to.val()
-        }
-    })
+    
     
     // $.fancybox.open({
     //     src: "#modal",
