5fa763d507039ce02863ea2577c6e093465a8ea7
Anton Afanasyev <anton-afanasyev@mail.ru>
2021-11-14 13:22:24 +0300

    правки по отправке запроса

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,52 +1,74 @@
 const validateFields = (form, fieldsArray) => {
-    fieldsArray.forEach((field) => {
-        field.removeClass("input-error");
-        if(field.val() === "") {
-            field.addClass("input-error");
-        }
+  fieldsArray.forEach((field) => {
+    field.removeClass("input-error");
+    if (field.val().trim() === "") {
+      field.addClass("input-error");
+    }
+  });
+
+  const errorFields = form.find(".input-error");
+
+  return errorFields.length === 0;
+};
+
+$(".form").submit((e) => {
+  e.preventDefault();
+
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");
+
+  const modal = $("#modal");
+  const content = modal.find(".modal__content");
+
+  modal.removeClass("error-modal");
+
+  const isValid = validateFields(form, [name, phone, comment, to]);
+
+  if (isValid) {
+    const request = $.ajax({
+      url: "https://httpbin.org/post",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      },
+      error: (data) => {},
     });
 
-    const errorFields = form.find(".input-error");
+    request.done((data) => {
+      content.text(data.message);
+    });
 
-    return errorFields.length === 0;
-}
+    request.fail((data) => {
+      const message = data.responseJSON.message;
+      content.text(message);
+      modal.addClass("error-modal");
+    });
 
-$('.form').submit(e => {
-    e.preventDefault();
-
-    const form = $(e.currentTarget);
-    const name = form.find("[name='name']");
-    const phone = form.find("[name='phone']");
-    const comment = form.find("[name='comment']");
-    const to = form.find("[name='to']");
-
-    const isValid = validateFields(form,[name, phone, comment, to]);
-
-
-    if(isValid){
-        $.ajax({
-            url: "https://webdev-api.loftschool.com/sendmail",
-            method: "post",
-            data: {
-                name: name.val(),
-                phone: phone.val(),
-                comment: comment.val(),
-                to: to.val(),
-            },
-        });
-    }
-    
-        
-    
-    
-    // $.fancybox.open({
-    //     src: "#modal",
-    //     type: "inline"
-    // })
+    request.always(() => {
+      $.fancybox.open({
+        src: "#modal",
+        type: "inline",
+      });
+    });
+  }
 });
 
-$(".app-submit-btn").click(e => {
-    e.preventDefault();
+$(".app-submit-btn").click((e) => {
+  e.preventDefault();
+  const form = $('form');
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']")
 
-    $.fancybox.close();
-})
+  comment.val('');
+  name.val("")
+  phone.val("")
+  
+  $.fancybox.close();
+});
