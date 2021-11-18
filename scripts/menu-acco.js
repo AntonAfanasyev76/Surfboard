8bd88838dc08a9cdcd5b0701569ebc82d1c1fed0
Anton Afanasyev <anton-afanasyev@mail.ru>
2021-11-18 21:40:51 +0300

    Update menu-acco.js

--- scripts/menu-acco.js
+++ scripts/menu-acco.js
@@ -1,34 +1,50 @@
 const mesureWidth = item => {
+    let reqItemWidth = 0;
+
     const screenWidth = $(window).width();
     const container = item.closest(".products-menu");
     const titlesBlocks = container.find(".products-menu__title");
     const titlesWidth = titlesBlocks.width() * titlesBlocks.length;
 
+    const textContainer = item.find(".products-menu__container");
+
+    const paddingLeft = parseInt(textContainer.css("padding-left"));
+    const paddingRight = parseInt(textContainer.css("padding-right"));
+    
     const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-    if (isMobile){
-        return (screenWidth - titlesWidth);
+    if (isMobile) {
+        reqItemWidth = screenWidth - titlesWidth;
     } else {
-        return 500;
+        reqItemWidth = 500;
     }
 
-    
-    
-}
+    return {
+        container: reqItemWidth,
+        textContainer: reqItemWidth - paddingRight
+    };
+
+};
 
 const closeEveryItemInContainer = container => {
     const items = container.find(".products-menu__item");
     items.removeClass("active");
     const content = container.find(".products-menu__content");
     content.width(0);
-}
+};
 
 const openItemMenu = item => {
+    
     const hiddenContent = item.find(".products-menu__content");
     const reqWidth = mesureWidth(item);
+    const textBlock = item.find(".products-menu__container");
+    
     item.addClass("active");
-    hiddenContent.width(reqWidth);
-}
+    hiddenContent.width(reqWidth.container);
+    console.log(reqWidth.container);
+    textBlock.width(reqWidth.textContainer);
+    console.log(reqWidth.textContainer);
+};
 
 $(".products-menu__title").on("click", e => {
     e.preventDefault();
@@ -37,8 +53,8 @@ $(".products-menu__title").on("click", e => {
     const item = $this.closest(".products-menu__item");
     const itemOpened = item.hasClass("active");
     const container = $this.closest(".products-menu");
-    
-    if(itemOpened) {
+
+    if (itemOpened) {
         closeEveryItemInContainer(container);
     } else {
         closeEveryItemInContainer(container);
