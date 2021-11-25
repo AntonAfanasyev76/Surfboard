c4634ad7f31713a52a4f14c07dc4606e3abd84ac
Афанасьев А.О <@centerb.local>
2021-11-25 12:02:55 +0300

    реализация склейки и минификации файлов скриптов

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 const {src, dest, task} = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 // function copy() {
 //     return src('scripts/*.js').pipe(dest('dist'))
 // }
 
-task('scripts', () => {
+function scripts() {
     return src('scripts/*.js')
-    .pipe(concat("main.js"))
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
     .pipe(dest("dist"))
-})
+}
 
-// exports.copy = copy;
+exports.scripts = scripts;
+
+// task("default", series("scripts"));
